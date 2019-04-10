7af92f5fa888112a189344b2097315a9955a4c20
sunye3 <sun.ye3@iwhalecloud.com>
2019-04-10 14:49:32 +0800

    Initial commit

---
+++ src/main/java/xyz/likersai/awi/AwiApplication.java
@@ -0,0 +1,13 @@
+package xyz.likersai.awi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwiApplication.class, args);
+    }
+
+}
